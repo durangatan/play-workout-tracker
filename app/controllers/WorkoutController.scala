1bf63c2eca916c3c866ab116c62f345b38e357f5
duranje <joeduran8@gmail.com>
2019-01-11 15:34:07 -0600

    allow exit from workout

--- app/controllers/WorkoutController.scala
+++ app/controllers/WorkoutController.scala
@@ -9,9 +9,9 @@ import scala.util.{Try, Success, Failure}
 @Singleton
 class WorkoutController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def endScreen() = Action { implicit request: Request[AnyContent] => {
-    val (totalWeight, totalSets) = EastBankClubSets.default.
-      foldLeft[Tuple2[Int, Int]]((0, 0))((acc, set) => (acc._1 + set.weight, acc._2 + 1))
+  def endScreen(lastSetIndex: Int) = Action { implicit request: Request[AnyContent] => {
+    val (totalWeight, totalSets) = EastBankClubSets.default.take(lastSetIndex).
+      foldLeft[Tuple2[Int, Int]]((0, 0))((acc, set) => (acc._1 + set.reps * set.weight, acc._2 + 1))
     Ok(views.html.endscreen(totalWeight, totalSets))
     }
   }
